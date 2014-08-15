f535ffee0b985c0d45d46777a21569239d86eb2d
Kevin Martensson <kevinmartensson@gmail.com>
2014-08-15 21:03:47 +0200

    Use ava for testing

--- test/test.js
+++ test/test.js
@@ -1,23 +1,25 @@
-/*global describe, it */
 'use strict';
 
-var assert = require('assert');
 var ExecBuffer = require('../');
 var fs = require('fs');
 var gifsicle = require('gifsicle').path;
 var path = require('path');
+var test = require('ava');
 
-describe('execBuffer()', function () {
-    it('should return a optimized Buffer', function (cb) {
-        var buf = fs.readFileSync(path.join(__dirname, 'fixtures/test.gif'));
-        var execBuffer = new ExecBuffer();
+test('should return a optimized Buffer', function (t) {
+    t.plan(4);
+
+    var execBuffer = new ExecBuffer();
+
+    fs.readFile(path.join(__dirname, 'fixtures/test.gif'), function (err, buf) {
+        t.assert(!err);
 
         execBuffer
             .use(gifsicle, ['-o', execBuffer.dest(), execBuffer.src()])
             .run(buf, function (err, data) {
-                assert(data.length > 0);
-                assert(data.length < buf.length);
-                cb();
+                t.assert(!err);
+                t.assert(data.length > 0);
+                t.assert(data.length < buf.length);
             });
     });
 });
